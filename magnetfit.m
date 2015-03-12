8e2555d4f48d80fdbd7898902fbc6ee2b83d41ef
Dave Lu <dave792321001730003212345@gmail.com>
2015-03-12 00:05:10 -0400

    predict using fourier2

--- magnetfit.m
+++ magnetfit.m
@@ -1,38 +1,43 @@
 clear all; clc; close all;
 
+fitmodel = 'fourier2';
+
 data = csvread('Data/magnet1.csv');
 
-t0 = 1.03; tend = 23.03;
-t = (t0:0.01:tend)';
-angle_top = interp1(data(:,1)/1000, data(:,2), t);
-angle_bot = interp1(data(:,3)/1000, data(:,4), t);
+t0 = 2; tend = 23;
+ts = (t0:0.1:tend)';
+angle_top = interp1(data(:,1)/1000, data(:,2), ts);
+angle_bot = interp1(data(:,3)/1000, data(:,4), ts);
 angle_knee = angle_top-angle_bot;
-t = t - t0;
+ts = ts - t0;
 
-angle_knee_original = angle_knee;
+start = 33; stop = 53;
+ts_slice = ts(start:stop);
+angle_knee_slice = angle_knee(start:stop);
 
-angle_knee(angle_knee < 0.15) = 0.15;
+predicted = fit(ts_slice, angle_knee_slice, fitmodel);
 
-swinging = [0; diff(angle_knee)];
-swinging(swinging < 0) = -1;
-swinging(swinging > 0) = 1;
-
-
-figure;
-plot( ...
-   t, angle_top, ...
-   t, angle_bot, ...
-   t, angle_knee, ...
-   t, angle_knee_original ...
-);
-   % t, swinging ...
-legend('top', 'bot', 'top-bot');
+figure; hold on;
+plot(ts, angle_knee, '--');
+plot(ts_slice, angle_knee_slice, 'Color', [0 0.5 0]);
+plot(predicted);
+legend('everything', 'slice', 'predicted');
 xlim([0 tend-t0]);
 
-start = 700; stop = 790;
-t = t(start:stop);
-angle_knee = angle_knee(start:stop);
+window = 20;
+forecast = 0.2;
+predictions = zeros(length(ts),1);
+for i = (window+1):length(ts)
+   t = ts(i);
+   fitted = fit(ts(i-window:i), angle_knee(i-window:i), fitmodel);
+   predictions(i) = fitted(t+forecast);
+end
+predictions(predictions < 0) = 0;
+
 figure;
 plot( ...
-   t, angle_knee ...
+   ts, angle_knee, ...
+   ts+forecast, predictions ...
 );
+legend('actual', 'predicted');
+xlim([0 tend-t0]);
